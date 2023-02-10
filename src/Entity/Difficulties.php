80817b335d812fbbd0536004f5e7316f1213dfae
Rebecca GERREY <rebeccag@live.fr>
2023-02-10 18:58:24 +0100

    Difficulties entity created + migration done.

---
+++ src/Entity/Difficulties.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\DifficultiesRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ORM\Entity(repositoryClass: DifficultiesRepository::class)]
+class Difficulties
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer', unique: true)]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    #[Assert\NotBlank(message: 'Veuillez renseigner un nom de difficulté.')]
+    private ?string $name = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+}
+
